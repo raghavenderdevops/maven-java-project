d952b68c7870f5310f5157cf3bfe993e2e745a6e
raghavenderdevops <103017977+raghavenderdevops@users.noreply.github.com>
2022-04-11 16:37:21 +0530

    Update App.java
    poll scm test update

--- src/main/java/com/raghu/app/App.java
+++ src/main/java/com/raghu/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! bsdkshibdfgbayftufafg" );
+        System.out.println( "Hello World! bsdkshibdfgbayftufafg  poll scm test update " );
     }
 }
