e86f3c2b2df97599a913b20016f020b696d1948d
raghavender <103017977+raghavenderdevops@users.noreply.github.com>
2022-08-04 15:05:02 +0530

    Update App.java

--- src/main/java/com/raghu/app/App.java
+++ src/main/java/com/raghu/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! bsdkshibdfgbayftufafg  poll scm test update " );
+        System.out.println( " Webappclouds Latest Deployment " );
     }
 }
